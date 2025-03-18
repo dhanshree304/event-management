f62f25c74e146fcae82962ad16e8d2a6bf186ad4
dhanshree304 <dhanshreeupadhye0@gmail.com>
2025-03-18 21:48:47 +0530

    deploy it

--- fronted/vite.config.js
+++ fronted/vite.config.js
@@ -6,7 +6,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/event-management",
+  base:"/event-management/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
